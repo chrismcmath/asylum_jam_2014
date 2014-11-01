4c80314a140a7e0b34cbc5c520afcdf1ee8f3a1d
Chris McMath <chrismcmath@substantial-games.com>
2014-11-01 14:52:55 +0800

    Action button listener

---
+++ Assets/Scripts/Player/ActionButtonListener.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionButtonListener : MonoBehaviour {
+	public void Update () {
+        if (Input.GetButtonUp("Action")) {
+            Debug.Log("ACtion button pressed");
+        }
+	}
+}
