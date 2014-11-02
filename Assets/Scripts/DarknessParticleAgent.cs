b4a93ef5599286f27b1c81d9d6e3c6a76f2aef96
Tobias Baumann <m@tobiasbaumann.net>
2014-11-02 16:20:02 +0800

    doors and darkness updated

---
+++ Assets/Scripts/DarknessParticleAgent.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// whenever we turn on this script, stop the particle emition and go back home
+
+public class DarknessParticleAgent : MonoBehaviour {
+	public Transform HomeTarget;
+	public NavMeshAgent Agent;
+	public ParticleSystem ps;
+	private float AgentBackSpeed;
+	
+	void Awake(){
+		
+	}
+	
+	// Use this for initialization
+	void Start () {
+		Agent = GetComponent<NavMeshAgent>();
+		Agent.enabled = true;
+		HomeTarget = GameObject.Find("Darkness Start").transform;
+		AgentBackSpeed = Agent.speed *2;
+		transform.parent = null;
+		ps.enableEmission = false;
+
+		Destroy(gameObject, 25.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Agent.SetDestination(HomeTarget.position);
+		Agent.speed = AgentBackSpeed;
+	}
+}
